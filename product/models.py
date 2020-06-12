609e8580be1650a35394051fb4fdb6080bb5f2f6
ing <inyoman_gurnitha@yahoo.com>
2020-06-12 15:53:31 +0700

    2. Created, registered Product model, and added a product

--- product/models.py
+++ product/models.py
@@ -1,3 +1,53 @@
+# product/models.py
 from django.db import models
 
-# Create your models here.
+# model name: Product
+class Product(models.Model):
+
+	## Each product has its own condition (new or used)
+	CONDITION_TYPE = (
+		("New", "New"),
+		("Used", "Used")
+	)
+
+	## It contains all the products information
+	name 			= models.CharField(max_length=100)
+	description = models.TextField(max_length=500)
+	condition 	= models.CharField(max_length=100, choices=CONDITION_TYPE)
+	price 		= models.DecimalField(max_digits=10, decimal_places=2)
+	created 		= models.DateTimeField()
+
+	def __str__(self):
+		return self.name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+1NF: Product
+============
+name, description, condition, price, published
+
+2NF: Product
+============
+name, description, condition(new, used) price, published
+
+
+
+
+
+
+
+
+
+'''
