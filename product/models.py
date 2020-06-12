846d5e847d6548b6ff04e1b5271283967a8f605c
ing <inyoman_gurnitha@yahoo.com>
2020-06-12 20:07:06 +0700

    4. Created Brand, ProductImages models and added product images

--- product/models.py
+++ product/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
 	
 	## It contains information about product Category
 	category_name 	= models.CharField(max_length=50)
-	image 			= models.ImageField(upload_to='products/', blank=True, null=True)
+	image 			= models.ImageField(upload_to='category/', blank=True, null=True)
 	
 	## Making correct plural of 'Categorys' to 'Categories'
 	class Meta:
@@ -36,6 +36,7 @@ class Product(models.Model):
 	description = models.TextField(max_length=500)
 	condition 	= models.CharField(max_length=100, choices=CONDITION_TYPE)
 	category 	= models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+	brand 		= models.ForeignKey('Brand' , on_delete=models.SET_NULL , null=True)
 	price 		= models.DecimalField(max_digits=10, decimal_places=2)
 	created 		= models.DateTimeField(default=timezone.now)
 
@@ -44,10 +45,30 @@ class Product(models.Model):
 
 
 
+# model name: Brand
+class Brand(models.Model):
+    ## for product brand
+    brand_name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'brand'
+        verbose_name_plural = 'brands'
+
+    def __str__(self):
+        return self.brand_name	
 
 
+# model name: ProductImages
+class ProductImages(models.Model):
+    product = models.ForeignKey(Product , on_delete=models.CASCADE)
+    image 	= models.ImageField(upload_to='products/', blank=True , null=True)
 
+    class Meta:
+        verbose_name = 'Product Image'
+        verbose_name_plural = 'Product Images'        
 
+    def __str__(self):
+        return self.product.name
 
 
 
